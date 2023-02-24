464490a6105a4205f92b028bb90635dc90527687
yashgulati54 <yashgulati54@gmail.com>
2023-02-24 22:47:37 +0530

    created tables
    created tables of admin, category, complaintremark and state

---
+++ db/cms.sql
@@ -0,0 +1,57 @@
+-- phpMyAdmin SQL Dump
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+-- Database: `cms`
+
+-- Table structure for table `admin`
+
+CREATE TABLE `admin` (
+  `id` int(11) NOT NULL,
+  `username` varchar(250) NOT NULL,
+  `password` varchar(250) NOT NULL,
+  `updationDate` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+-- Table structure for table `category`
+
+CREATE TABLE `category` (
+  `id` int(11) NOT NULL,
+  `categoryName` varchar(255) NOT NULL,
+  `categoryDescription` longtext NOT NULL,
+  `creationDate` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updationDate` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+-- Table structure for table `complaintremark`
+
+CREATE TABLE `complaintremark` (
+  `id` int(11) NOT NULL,
+  `complaintNumber` int(11) NOT NULL,
+  `status` varchar(255) NOT NULL,
+  `remark` mediumtext NOT NULL,
+  `remarkDate` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+-- Table structure for table `state`
+
+CREATE TABLE `state` (
+  `id` int(11) NOT NULL,
+  `stateName` varchar(255) NOT NULL,
+  `stateDescription` tinytext NOT NULL,
+  `postingDate` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updationDate` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+

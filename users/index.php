179f58aaea23843ec5baa1f038cc5be150621cd8
yashrajsingh317 <yashrajsingh317@gmail.com>
2023-03-06 17:22:48 +0530

    Updated index.php file.

--- users/index.php
+++ users/index.php
@@ -30,3 +30,23 @@ $extra="login.php";
 
 }
 }
+
+if(isset($_POST['change']))
+{
+   $email=$_POST['email'];
+    $contact=$_POST['contact'];
+    $password=md5($_POST['password']);
+$query=mysqli_query($bd, "SELECT * FROM users WHERE userEmail='$email' and contactNo='$contact'");
+$num=mysqli_fetch_array($query);
+if($num>0)
+{
+mysqli_query($bd, "update users set password='$password' WHERE userEmail='$email' and contactNo='$contact' ");
+$msg="Password Changed Successfully";
+
+}
+else
+{
+$errormsg="Invalid email id or Contact no";
+}
+}
+?>

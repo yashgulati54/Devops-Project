9a870bf230eb2cac46ed99c861fbe9545973aaea
Yash Gulati <yashgulati54@gmail.com>
2023-03-13 23:01:35 +0530

    created index.php file for admin portal

---
+++ admin/index.php
@@ -0,0 +1,31 @@
+<?php
+session_start();
+error_reporting(0);
+include("include/config.php");
+if(isset($_POST['submit']))
+{
+	$username=$_POST['username'];
+	$password=md5($_POST['password']);
+$ret=mysqli_query($bd, "SELECT * FROM admin WHERE username='$username' and password='$password'");
+$num=mysqli_fetch_array($ret);
+if($num>0)
+{
+$extra="change-password.php";//
+$_SESSION['alogin']=$_POST['username'];
+$_SESSION['id']=$num['id'];
+$host=$_SERVER['HTTP_HOST'];
+$uri=rtrim(dirname($_SERVER['PHP_SELF']),'/\\');
+header("location:http://$host$uri/$extra");
+exit();
+}
+else
+{
+$_SESSION['errmsg']="Invalid username or password";
+$extra="index.php";
+$host  = $_SERVER['HTTP_HOST'];
+$uri  = rtrim(dirname($_SERVER['PHP_SELF']),'/\\');
+header("location:http://$host$uri/$extra");
+exit();
+}
+}
+?>

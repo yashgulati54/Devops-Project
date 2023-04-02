634b7a0991ec4a810a35c58abf2f6aecfc8d7e75
Aman Kumar <amankumarbd@gmail.com>
2023-04-02 22:41:04 +0530

    added front-end to userprofile.php

--- admin/userprofile.php
+++ admin/userprofile.php
@@ -30,10 +30,96 @@ window.print();
 
 <div style="margin-left:50px;">
  <form name="updateticket" id="updateticket" method="post"> 
-<table width="100%" border="0" cellspacing="0" cellpadding="0">
+<table width="100%" cellspacing="0" cellpadding="0">
 <?php 
 
 $ret1=mysqli_query($bd, "select * FROM users where id='".$_GET['uid']."'");
 while($row=mysqli_fetch_array($ret1))
 {
 ?>
+
+<tr>
+      <td colspan="2"><b><?php echo $row['fullName'];?>'s profile</b></td>
+      
+    </tr>
+    
+    
+    <tr>
+      <td  >&nbsp;</td>
+      <td >&nbsp;</td>
+    </tr>
+    <tr height="50">
+      <td><b>Reg Date:</b></td>
+      <td><?php echo htmlentities($row['regDate']); ?></td>
+    </tr>
+    <tr height="50">
+      <td><b>User Email:</b></td>
+      <td><?php echo htmlentities($row['userEmail']); ?></td>
+    </tr>
+
+
+      <tr height="50">
+      <td><b>User Contact no:</b></td>
+      <td><?php echo htmlentities($row['contactNo']); ?></td>
+    </tr>
+    
+
+
+        <tr height="50">
+      <td><b>Address:</b></td>
+      <td><?php echo htmlentities($row['address']); ?></td>
+    </tr>
+
+
+
+        <tr height="50">
+      <td><b>State:</b></td>
+      <td><?php echo htmlentities($row['State']); ?></td>
+    </tr>
+
+
+        <tr height="50">
+      <td><b>Country:</b></td>
+      <td><?php echo htmlentities($row['country']); ?></td>
+    </tr>
+
+
+        <tr height="50">
+      <td><b>Pincode:</b></td>
+      <td><?php echo htmlentities($row['pincode']); ?></td>
+    </tr>  
+
+
+        <tr height="50">
+      <td><b>Last Updation:</b></td>
+      <td><?php echo htmlentities($row['updationDate']); ?></td>
+    </tr>
+     <tr height="50">
+      <td><b>Status:</b></td>
+      <td><?php if($row['status']==1)
+      { echo "Active";
+} else{
+  echo "Block";
+}
+        ?></td>
+    </tr>
+    
+    <tr>
+  
+      <td colspan="2">   
+      <input name="Submit2" type="submit" class="txtbox4" value="Close this window " onClick="return f2();" style="cursor: pointer;"  /></td>
+    </tr>
+   
+    <?php } 
+
+ 
+    ?>
+ 
+</table>
+ </form>
+</div>
+
+</body>
+</html>
+
+     <?php } ?>

149508b62de72b53ee6ebc7eb8b62313fa3115b2
Aman Kumar <amankumarbd@gmail.com>
2023-04-10 22:55:13 +0530

    error resolve

--- admin/edit-state.php
+++ admin/edit-state.php
@@ -21,6 +21,87 @@ $_SESSION['msg']="State info Updated !!";
 
 }
 
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Admin| State</title>
+	<link type="text/css" href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
+	<link type="text/css" href="bootstrap/css/bootstrap-responsive.min.css" rel="stylesheet">
+	<link type="text/css" href="css/theme.css" rel="stylesheet">
+	<link type="text/css" href="images/icons/css/font-awesome.css" rel="stylesheet">
+	<link type="text/css" href='http://fonts.googleapis.com/css?family=Open+Sans:400italic,600italic,400,600' rel='stylesheet'>
+</head>
+<body>
+<?php include('include/header.php');?>
+
+	<div class="wrapper">
+		<div class="container">
+			<div class="row">
+<?php include('include/sidebar.php');?>				
+			<div class="span9">
+					<div class="content">
+
+						<div class="module">
+							<div class="module-head">
+								<h3>State</h3>
+							</div>
+							<div class="module-body">
+
+									<?php if(isset($_POST['submit']))
+{?>
+									<div class="alert alert-success">
+										<button type="button" class="close" data-dismiss="alert">×</button>
+									<strong>Well done!</strong>	<?php echo htmlentities($_SESSION['msg']);?><?php echo htmlentities($_SESSION['msg']="");?>
+									</div>
+<?php } ?>
+
+
+									<br />
+
+			<form class="form-horizontal row-fluid" name="Category" method="post" >
+<?php
+$id=intval($_GET['id']);
+$query=mysqli_query($bd, "select * from state where id='$id'");
+while($row=mysqli_fetch_array($query))
+{
+?>									
+<div class="control-group">
+<label class="control-label" for="basicinput">State Name</label>
+<div class="controls">
+<input type="text" placeholder="Enter category Name"  name="state" value="<?php echo  htmlentities($row['stateName']);?>" class="span8 tip" required>
+</div>
+</div>
+
+
+<div class="control-group">
+											<label class="control-label" for="basicinput">Description</label>
+											<div class="controls">
+<textarea class="span8" name="description" rows="5"><?php echo  htmlentities($row['stateDescription']);?></textarea></div>
+										</div>
+									<?php } ?>	
+
+	<div class="control-group">
+											<div class="controls">
+												<button type="submit" name="submit" class="btn">Update</button>
+											</div>
+										</div>
+									</form>
+							</div>
+						</div>
+
+
+						
+
+						
+						
+					</div><!--/.content-->
+				</div><!--/.span9-->
+			</div>
+		</div><!--/.container-->
+	</div><!--/.wrapper-->
 
 <?php include('include/footer.php');?>
 
--- admin/state.php
+++ admin/state.php
@@ -28,6 +28,128 @@ if(isset($_GET['del']))
 
 ?>
 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Admin| State</title>
+	<link type="text/css" href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
+	<link type="text/css" href="bootstrap/css/bootstrap-responsive.min.css" rel="stylesheet">
+	<link type="text/css" href="css/theme.css" rel="stylesheet">
+	<link type="text/css" href="images/icons/css/font-awesome.css" rel="stylesheet">
+	<link type="text/css" href='http://fonts.googleapis.com/css?family=Open+Sans:400italic,600italic,400,600' rel='stylesheet'>
+</head>
+<body>
+<?php include('include/header.php');?>
+
+	<div class="wrapper">
+		<div class="container">
+			<div class="row">
+<?php include('include/sidebar.php');?>				
+			<div class="span9">
+					<div class="content">
+
+						<div class="module">
+							<div class="module-head">
+								<h3>State</h3>
+							</div>
+							<div class="module-body">
+
+									<?php if(isset($_POST['submit']))
+{?>
+									<div class="alert alert-success">
+										<button type="button" class="close" data-dismiss="alert">×</button>
+									<strong>Well done!</strong>	<?php echo htmlentities($_SESSION['msg']);?><?php echo htmlentities($_SESSION['msg']="");?>
+									</div>
+<?php } ?>
+
+
+									<?php if(isset($_GET['del']))
+{?>
+									<div class="alert alert-error">
+										<button type="button" class="close" data-dismiss="alert">×</button>
+									<strong>Oh snap!</strong> 	<?php echo htmlentities($_SESSION['delmsg']);?><?php echo htmlentities($_SESSION['delmsg']="");?>
+									</div>
+<?php } ?>
+
+									<br />
+
+			<form class="form-horizontal row-fluid" name="Category" method="post" >
+									
+<div class="control-group">
+<label class="control-label" for="basicinput">State Name</label>
+<div class="controls">
+<input type="text" placeholder="Enter State Name"  name="state" class="span8 tip" required>
+</div>
+</div>
+
+
+<div class="control-group">
+											<label class="control-label" for="basicinput">Description</label>
+											<div class="controls">
+												<textarea class="span8" name="description" rows="5"></textarea>
+											</div>
+										</div>
+
+	<div class="control-group">
+											<div class="controls">
+												<button type="submit" name="submit" class="btn">Create</button>
+											</div>
+										</div>
+									</form>
+							</div>
+						</div>
+
+
+	<div class="module">
+							<div class="module-head">
+								<h3>Manage States</h3>
+							</div>
+							<div class="module-body table">
+								<table cellpadding="0" cellspacing="0" border="0" class="datatable-1 table table-bordered table-striped	 display" width="100%">
+									<thead>
+										<tr>
+											<th>#</th>
+											<th>State</th>
+											<th>Description</th>
+											<th>Creation date</th>
+											<th>Last Updated</th>
+											<th>Action</th>
+										</tr>
+									</thead>
+									<tbody>
+
+<?php $query=mysqli_query($bd, "select * from state");
+$cnt=1;
+while($row=mysqli_fetch_array($query))
+{
+?>									
+										<tr>
+											<td><?php echo htmlentities($cnt);?></td>
+											<td><?php echo htmlentities($row['stateName']);?></td>
+											<td><?php echo htmlentities($row['stateDescription']);?></td>
+											<td> <?php echo htmlentities($row['postingDate']);?></td>
+											<td><?php echo htmlentities($row['updationDate']);?></td>
+											<td>
+											<a href="edit-state.php?id=<?php echo $row['id']?>" ><i class="icon-edit"></i></a>
+											<a href="state.php?id=<?php echo $row['id']?>&del=delete" onClick="return confirm('Are you sure you want to delete?')"><i class="icon-remove-sign"></i></a></td>
+										</tr>
+										<?php $cnt=$cnt+1; } ?>
+										
+								</table>
+							</div>
+						</div>						
+
+						
+						
+					</div><!--/.content-->
+				</div><!--/.span9-->
+			</div>
+		</div><!--/.container-->
+	</div><!--/.wrapper-->
+
+
 
 <?php include('include/footer.php');?>
 

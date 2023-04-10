2eed912cf70c3399fc158bd38ddfbe7e58412ff3
Aman Kumar <amankumarbd@gmail.com>
2023-04-10 22:42:50 +0530

    error resolve

--- admin/manage-users.php
+++ admin/manage-users.php
@@ -10,6 +10,101 @@ date_default_timezone_set('Asia/Kolkata');// change according timezone
 $currentTime = date( 'd-m-Y h:i:s A', time () );
 
 
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Admin| Manage Users</title>
+	<link type="text/css" href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
+	<link type="text/css" href="bootstrap/css/bootstrap-responsive.min.css" rel="stylesheet">
+	<link type="text/css" href="css/theme.css" rel="stylesheet">
+	<link type="text/css" href="images/icons/css/font-awesome.css" rel="stylesheet">
+	<link type="text/css" href='http://fonts.googleapis.com/css?family=Open+Sans:400italic,600italic,400,600' rel='stylesheet'>
+		<script language="javascript" type="text/javascript">
+var popUpWin=0;
+function popUpWindow(URLStr, left, top, width, height)
+{
+ if(popUpWin)
+{
+if(!popUpWin.closed) popUpWin.close();
+}
+popUpWin = open(URLStr,'popUpWin', 'toolbar=no,location=no,directories=no,status=no,menubar=no,scrollbars=yes,resizable=no,copyhistory=yes,width='+600+',height='+600+',left='+left+', top='+top+',screenX='+left+',screenY='+top+'');
+}
+
+</script>
+</head>
+<body>
+<?php include('include/header.php');?>
+
+	<div class="wrapper">
+		<div class="container">
+			<div class="row">
+<?php include('include/sidebar.php');?>				
+			<div class="span9">
+					<div class="content">
+
+	<div class="module">
+							<div class="module-head">
+								<h3>Manage Users</h3>
+							</div>
+							<div class="module-body table">
+	<?php if(isset($_GET['del']))
+{?>
+									<div class="alert alert-error">
+										<button type="button" class="close" data-dismiss="alert">×</button>
+									<strong>Oh snap!</strong> 	<?php echo htmlentities($_SESSION['delmsg']);?><?php echo htmlentities($_SESSION['delmsg']="");?>
+									</div>
+<?php } ?>
+
+									<br />
+
+							
+								<table cellpadding="0" cellspacing="0" border="0" class="datatable-1 table table-bordered table-striped	 display" width="100%">
+									<thead>
+										<tr>
+											<th>#</th>
+											<th> Name</th>
+											<th>Email </th>
+											<th>Contact no</th>
+											<th>Reg. Date </th>
+											<th>Action</th>
+										
+										</tr>
+									</thead>
+									<tbody>
+
+<?php $query=mysqli_query($bd, "select * from users");
+$cnt=1;
+while($row=mysqli_fetch_array($query))
+{
+?>									
+										<tr>
+											<td><?php echo htmlentities($cnt);?></td>
+											<td><?php echo htmlentities($row['fullName']);?></td>
+											<td><?php echo htmlentities($row['userEmail']);?></td>
+											<td> <?php echo htmlentities($row['contactNo']);?></td>
+										
+											<td><?php echo htmlentities($row['regDate']);?></td>
+
+											<td><a href="javascript:void(0);" onClick="popUpWindow('http://localhost/Complaint Management System/admin/userprofile.php?uid=<?php echo htmlentities($row['id']);?>');" title="Update order">
+											 <button type="button" class="btn btn-primary">View Detials</button>
+											</a></td>
+											
+										<?php $cnt=$cnt+1; } ?>
+										
+								</table>
+							</div>
+						</div>						
+
+						
+						
+					</div><!--/.content-->
+				</div><!--/.span9-->
+			</div>
+		</div><!--/.container-->
+	</div><!--/.wrapper-->
 
 <?php include('include/footer.php');?>
 
